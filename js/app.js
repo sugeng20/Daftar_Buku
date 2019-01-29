4b09681aa689f35a17e5e627a1d264c06747fced
Sugeng <sugeng.wanantara@gmail.com>
2019-01-30 05:10:05 +0700

    Slide Show

--- js/app.js
+++ js/app.js
@@ -1,24 +1,6 @@
-// $("document").ready(function () {
-//   $('#oke').click(function () {
-//     Swal.fire({
-//       title: 'Are you sure?',
-//       text: "You won't be able to revert this!",
-//       type: 'warning',
-//       showCancelButton: true,
-//       confirmButtonColor: '#3085d6',
-//       cancelButtonColor: '#d33',
-//       confirmButtonText: 'Yes, delete it!'
-//     }).then((result) => {
-//       if (result.value) {
-//         Swal.fire(
-//           'Deleted!',
-//           'Your file has been deleted.',
-//           'success'
-//         )
-//       }
-//     })
-//   })
-// })
+$('.carousel').carousel({
+  interval: 2000
+})
 
 new Vue({
   el: "#app",
